4e0627921ff2902cb626da4e4136c1c9015120ec
sardorbek2024 <168269584+sardorbek2024@users.noreply.github.com>
2024-08-29 11:14:00 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ let a = +prompt( "1 число")
 let с = prompt("напишите %")
 let b = +prompt("процент")
 
-if( с == "%" ){
+if( c == "+"){
+    alert( a + b)
+}else if( c == "-"){
+    alert( a - b)
+}else if( c == "*"){
+    alert( a * b)
+}else if( c == "/"){
+    alert( a / b)
+}else if( с == "%" ){
     alert(a * b / 100)
 }else{
     alert("динаху пидор")
